f435e65bbd90a80f5c6cbab2a7f118c03f445cda
albertofcasuso <albertofcasuso@gmail.com>
2019-10-25 17:28:01 -0500

    starting 1.12

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,105 +1,28 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Statistic = (props) =>{
-    const text = props.text
-    const stat = props.value
-
-    return(
-            <tr>
-                <td>{text}</td>
-                <td>{stat}</td>
-            </tr>
-    )
-}
-
-const Button = (props) => {
-    const text = props.text
-    const handleClick = props.handleClick
-    return(
-        <div>
-                <button onClick={handleClick}>{text}</button>
-        </div>
-    )
-}
-
-
-
-const Statistics = (props) => {
-    const good = props.good
-    const neutral = props.neutral
-    const bad = props.bad
-    const total = props.total
-    const average = props.average
-
-    if (total === 0) {
-    return (
-      <div>
-          <h1>Statistics</h1>
-        <p>No votes yet</p>
-      </div>
-    )
-  }
-
-    return(
-        <div>
-        <h1>Statistics</h1>
-        <table>
-            <tbody>
-            <Statistic text='Good' value={good}/>
-            <Statistic text='Bad' value={bad}/>
-            <Statistic text='Neutral' value={neutral}/>
-            <Statistic text='Total' value={total}/>
-            <Statistic text='Average' value={average/total}/>
-            <Statistic text='Positive %' value={(good/total)*100}/>
-            </tbody>
-        </table>
-        </div>
-    )
-}
-
-const App = () => {
-  // save clicks of each button to own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [total, setTotal] = useState(0)
-  const [average, setAverage] = useState(0)
-
-
-  const setG = () => () =>{
-      setGood(good+1)
-      setTotal(total+1)
-      setAverage(average+1)
-  }
-
-  const setN = () => () =>{
-      setNeutral(neutral+1)
-      setTotal(total+1)
-
-  }
-  const setB = () => () =>{
-      setBad(bad+1)
-      setTotal(total+1)
-      setAverage(average-1)
-
-  }
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
 
   return (
     <div>
-      <h1>give feedback</h1>
-      <Button text='good' handleClick={setG()}/>
-      <Button text='bad'handleClick={setB()}/>
-      <Button text='neutral'handleClick={setN()}/>
-
-      <Statistics good={good} neutral={neutral} bad={bad} total={total} average={average}/>
+      {props.anecdotes[selected]}
     </div>
   )
 }
 
-ReactDOM.render(<App />,
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
   document.getElementById('root')
-)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
