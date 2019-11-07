33fa2bdfd7f3ee87a1a93d6747f28b7b727ca370
albertofcasuso <casuso@tutanota.com>
2019-11-07 16:48:47 -0600

    Exercise 2.3 Finished

--- part2/src/components/Total.js
+++ part2/src/components/Total.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 
 const Total = (props) => {
+
   const {parts} = props
-    let a= 0
-    parts.forEach(value => {
-        a+=value.exercises
-    })
+  const exercises = parts.map((x) => x.exercises)
+  const total = exercises.reduce((s, p) => s + p)
+
     return (
-        <p>Total of exercises {a}</p>
+        <p>Total of exercises {total}</p>
     )
 }
 
