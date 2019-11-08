a41de39b73470f0ecf34772613aca71cc0ca33eb
albertofcasuso <casuso@tutanota.com>
2019-11-07 18:01:06 -0600

    added form and submit buttons

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,19 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Note from './components/Note'
 
-const App = ({notes}) => {
+const App = (props) => {
+  const {notes} = props
+  const [note, setNote] = useState(notes)
 
   const noteList = notes.map(
     nota =>
       <Note key={nota.id} note={nota}/>
     )
-
+  const addNote = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+  }
+  
   return (
     <div>
       <h1>Notes</h1>
         <ul>
           {noteList}
         </ul>
+        <form onSubmit={addNote}>
+          <input />
+          <button type="submit">save</button>
+        </form>
     </div>
   )
 }
