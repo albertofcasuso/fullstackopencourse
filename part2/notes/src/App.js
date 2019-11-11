41dfc16075865c152b3f56ef0204aa0be1a1cf16
albertofcasuso <casuso@tutanota.com>
2019-11-11 07:55:07 -0600

    Starting part 2 step 6

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,13 +1,30 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
+
 const App = (props) => {
-  const {notes} = props
-  const [note, setNote] = useState(notes)
+
+
+  const [note, setNote] = useState([])
   const [newNote, setNewNote] = useState('a new note...')
   const [showAll, setShowAll] = useState(true)
 
-  const notesToShow = showAll ? notes : notes.filter(note => note.important)
+  /*Use effect se ejecuta despues del render*/
+  const fetchData = () =>{
+    console.log('effect')
+    axios
+    .get('http://localhost:3001/notes')
+    .then(response => {
+      console.log('promise fulfilled')
+      setNote(response.data)
+    })
+  }
+  useEffect(fetchData,[])
+  /*Este clog se ejecuta al leer el programa*/
+  console.log('render', note.length, 'note')
+
+  const notesToShow = showAll ? note : note.filter(note => note.important)
 
   const noteList = notesToShow.map(
     nota =>
@@ -26,7 +43,7 @@ const App = (props) => {
     content: newNote,
     date: new Date().toISOString(),
     important: Math.random() > 0.5,
-    id: notes.length + 1,
+    id: note.length + 1,
   }
   setNote(note.concat(noteObject))
 
@@ -47,8 +64,10 @@ const App = (props) => {
           <input value={newNote} onChange={handleChange} />
           <button type="submit">save</button>
         </form>
+        {console.log('soy el html')}
     </div>
   )
 }
+
 
 export default App
