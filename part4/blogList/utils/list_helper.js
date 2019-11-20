aef1cd34b06861be52ba069208df0edabeba2835
albertofcasuso <casuso@tutanota.com>
2019-11-20 16:11:05 -0600

    fixed eslint issues

--- part4/blogList/utils/list_helper.js
+++ part4/blogList/utils/list_helper.js
@@ -1,15 +1,17 @@
 const dummy = (blogs) => {
+  if(blogs){
     return 1
   }
+}
 
 //TOTAL LIKES
 const totalLikes = (posts) => {
-    const reducer = (accumulator, currentValue) => accumulator + currentValue;
-    const likeArray = posts.map(post=>post.likes)
-    return likeArray.reduce(reducer)
+  const reducer = (accumulator, currentValue) => accumulator + currentValue
+  const likeArray = posts.map(post=>post.likes)
+  return likeArray.reduce(reducer)
 }
   
-  module.exports = {
-    dummy,
-    totalLikes
-  }
+module.exports = {
+  dummy,
+  totalLikes
+}
