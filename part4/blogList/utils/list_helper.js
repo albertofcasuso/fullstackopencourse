93ae8a42005f2551deb69e1cc046ed3977ceb764
albertofcasuso <casuso@tutanota.com>
2019-11-20 15:58:13 -0600

    4.4 step

---
+++ part4/blogList/utils/list_helper.js
@@ -0,0 +1,15 @@
+const dummy = (blogs) => {
+    return 1
+  }
+
+//TOTAL LIKES
+const totalLikes = (posts) => {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    const likeArray = posts.map(post=>post.likes)
+    return likeArray.reduce(reducer)
+}
+  
+  module.exports = {
+    dummy,
+    totalLikes
+  }
