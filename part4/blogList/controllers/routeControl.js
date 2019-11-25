a13313783222cc7edced3cd058c4fa451dc4e236
albertofcasuso <casuso@tutanota.com>
2019-11-25 09:43:02 -0600

    adding try catch blocks

--- part4/blogList/controllers/routeControl.js
+++ part4/blogList/controllers/routeControl.js
@@ -2,19 +2,24 @@ const routeControl = require('express').Router()
 const Blog = require('../models/blog')
 
 routeControl.get('/',async (request, response) => {
-  const blogs = await Blog.find({})
-  response.json(blogs.map(blog=>blog.toJSON()))
-  
+
+  try{
+    const blogs = await Blog.find({})
+    response.json(blogs.map(blog=>blog.toJSON()))
+  }catch(error){
+    console.log(error)
+  }
 })
 
-routeControl.post('/', (request, response) => {
+routeControl.post('/', async (request, response) => {
   const blog = new Blog(request.body)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+  try{
+    const result =  await blog.save()
+    response.status(201).json(result.toJSON())
+  }catch(error){
+    console.log(error)
+  }
 })
 
 module.exports = routeControl
