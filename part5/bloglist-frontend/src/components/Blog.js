9f66f18d51a5b1bb02f485fd618aebddc3e053c7
albertofcasuso <casuso@tutanota.com>
2019-12-05 12:01:00 -0600

    indentation fixes

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -2,25 +2,25 @@ import React from 'react'
 import Post from './Post'
 
 const Blog = (props) => {
-  const {blogs} = props
-  const {deleteHandler} = props
-  const {user} = props
+    const {blogs} = props
+    const {deleteHandler} = props
+    const {user} = props
 
-  const sortedBlogs = blogs.sort((a,b)=>(a.likes>b.likes?a:b))
-  const blogList = sortedBlogs.map(blog=>{
-    return (
-      <div key={blog.id}>
-        <Post postInfo = {blog} deleteHandler={deleteHandler} user={user}/>
-      </div>
-      )
-  })
+    const sortedBlogs = blogs.sort((a,b)=>(a.likes>b.likes?a:b))
+    const blogList = sortedBlogs.map(blog=>{
+        return (
+            <div key={blog.id}>
+                <Post postInfo = {blog} deleteHandler={deleteHandler} user={user}/>
+            </div>
+        )
+    })
 
-  return(
-    <div>
-      <h1>Blog List</h1>
-      {blogList}
-    </div>
-  )
-  }
+    return(
+        <div>
+            <h1>Blog List</h1>
+            {blogList}
+        </div>
+    )
+}
 
 export default Blog
