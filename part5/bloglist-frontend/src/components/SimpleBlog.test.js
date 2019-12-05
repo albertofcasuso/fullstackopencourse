41e08c27583cbbb69c1a0816e28e0bc314de41e3
albertofcasuso <casuso@tutanota.com>
2019-12-05 08:03:15 -0600

    5.14 button pressed twice test

--- part5/bloglist-frontend/src/components/SimpleBlog.test.js
+++ part5/bloglist-frontend/src/components/SimpleBlog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import {render} from '@testing-library/react'
+import {render, fireEvent} from '@testing-library/react'
 import SimpleBlog from '../components/SimpleBlog'
 
 const blog = {
@@ -10,10 +10,19 @@ const blog = {
 }
 
 const component = render(<SimpleBlog blog={blog}/>)
+const mockHandler = jest.fn()
 
 test('renders title', ()=>{
     const component = render(<SimpleBlog blog={blog}/>)
     expect(component.container).toHaveTextContent('The cool title')
     expect(component.container).toHaveTextContent('the star author')
     expect(component.container).toHaveTextContent(55)
+})
+
+test('clicking the button works', ()=>{
+    const {getByText} = render(<SimpleBlog blog={blog} onClick={mockHandler}/>)
+    const button = getByText('like')
+    fireEvent.click(button)
+    fireEvent.click(button)
+    expect(mockHandler.mock.calls.length).toBe(2)
 })
