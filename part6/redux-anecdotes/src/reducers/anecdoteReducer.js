4bb119878a65ece7405ee147266a299d78ac6c7a
Alberto <albertofcasuso@gmail.com>
2019-12-11 13:03:06 -0600

    cleanup

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,14 +1,5 @@
 /* eslint-disable default-case */
 
-const getId = () => (100000 * Math.random()).toFixed(0)
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  }
-}
 export const createAnecdote = (content) => {
   return {
     type: 'NEW_ANECDOTE',
