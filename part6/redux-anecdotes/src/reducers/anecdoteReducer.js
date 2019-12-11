dd3bed13eabfba4a544c0cc9914cd830decb2df3
Alberto <albertofcasuso@gmail.com>
2019-12-11 12:37:26 -0600

    6.16 finished

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,12 +1,4 @@
 /* eslint-disable default-case */
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
 
 const getId = () => (100000 * Math.random()).toFixed(0)
 
@@ -30,9 +22,15 @@ export const voteAnecdote = (id) => {
     data:{id}
   }
 }
-const initialState = anecdotesAtStart.map(asObject)
 
-const anecdoteReducer = (state = initialState, action) => {
+export const initializeAnecdotes = (anecdotes) =>{
+  return{
+    type:'INIT_ANECDOTES',
+    data:anecdotes
+  }
+}
+
+const anecdoteReducer = (state = [], action) => {
 
   switch (action.type) {
     case 'VOTE':
@@ -42,6 +40,8 @@ const anecdoteReducer = (state = initialState, action) => {
       return stateToReturn
     case 'NEW_ANECDOTE':
       return [...state,asObject(action.data.content)]
+    case 'INIT_ANECDOTES':
+      return action.data
   }
   return state
 }
